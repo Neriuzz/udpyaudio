40d9246c78827379d6d8d1d57f5173c438f6c274
Neriuzz <neriusilmonas@gmail.com>
2020-02-05 15:42:07 +0000

    Improve program structure and update README

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # udpyaudio
 Scrapes Urban Dictionary for a given search term and downloads all the available audio pronunciations of that seach term.
 
-python udpyaudio.py [SEARCH_TERM]
+```bash
+python3 udpyaudio.py [SEARCH_TERM]
+```
--- udpyaudio.py
+++ udpyaudio.py
@@ -1,52 +1,59 @@
-#!/usr/bin/evn python3
+#!/usr/bin/env python3
 
 from bs4 import BeautifulSoup as bs4
-import urllib2, os, sys
+from urllib.request import urlopen
+import sys
+import os
 
 BASE_URL = 'https://www.urbandictionary.com/define.php?term='
 
+
+def change_to_dir(search_term):
+    PATH = f'udpyaudio/{search_term}'
+    if not os.path.exists(PATH):
+        os.makedirs(PATH)
+
+    os.chdir(PATH)
+
+
+def get_sound_urls(content):
+    SOUP = bs4(content, features='html.parser')
+    try:
+        return SOUP.findAll(
+            'a', {'class': 'play-sound'})[1]['data-urls'].strip('[]').split(',')
+    except:
+        print('[!] Word does not have any downloadable audio pronunciations')
+        sys.exit(1)
+
+
+def get_content(url):
+    try:
+        return urlopen(url).read()
+    except:
+        print('[!] Word cannot be found in the Urban Dictionary')
+        sys.exit(1)
+
+
 def download_audio(url, search_term):
+    sound_urls = get_sound_urls(get_content(url))
+    change_to_dir(search_term)
 
-    try:
-        content = urllib2.urlopen(url).read()
-    except:
-        print('No such term exists in the Urban Dictionary!')
-        sys.exit(1)
-
-    soup = bs4(content, features="html.parser")
-
-    try:
-        sound_urls = soup.findAll('a', {'class': 'play-sound'})[1]['data-urls'].strip('[]').split(',')
-    except:
-        print('No audio pronunciation found for this search term')
-        sys.exit(1)
-
-    path = r'udpyaudio/{}'.format(search_term)
-
-
-    if not os.path.exists(path):
-        os.makedirs(path)
-    
-    os.chdir(path)
-
-    i = 0
-    for url in sound_urls:
-        mp3file = urllib2.urlopen(url.strip('"'))
+    print('[!] Starting download...')
+    for i, url in enumerate(sound_urls):
+        mp3file = urlopen(url.strip('"'))
         with open('{}{}.mp3'.format(search_term, i), 'wb') as output:
-            print('Saving file {}/{}'.format(i + 1, len(sound_urls)))
+            print('[+] Downloading {}/{}'.format(i + 1, len(sound_urls)))
             output.write(mp3file.read())
-        i += 1
 
 
 if __name__ == '__main__':
     os.chdir(os.path.expanduser('~/Documents'))
     search_term = ' '.join(sys.argv[1:])
-
     if search_term:
         url = (BASE_URL + search_term).replace(' ', '%20')
         download_audio(url, search_term)
-        print('Finished downloading audio files!')
+        print('[!] Finished downloading audio files!')
         sys.exit(0)
     else:
-        print('Please provide a search term')
+        print('[!] Please provide a search term')
         sys.exit(1)
